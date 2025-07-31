336ac2a3ffaba6d6132357a357af19d1c0892029
Vitor Pedro <vtor_11@hotmail.com>
2025-07-31 04:15:06 +0000

    Nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -1,62 +1,87 @@
 #include <stdio.h>
 
+/// Função recursiva para mover torre
+void movTorre(int casas){
+    if (casas == 0)return;
+    printf("Direita\n");
+    movTorre(casas - 1);
+}
+
+/// função recursiva para mover bispo
+void movBispo(int casas){
+    if (casas == 0)return;
+    printf("Cima, Direita\n");
+    movBispo(casas - 1);
+}
+
+/// Função recursiva para mover rainha
+void movRainha(int casas){
+    if (casas == 0)return;
+    printf("Esquerda\n");
+    movRainha(casas - 1);
+}
+
+/// Função bispo com loops aninhados
+void movBispoAni(int cima, int direita){
+    printf("\nMovimento do Bispo\n");
+    for(int i = 0; i < cima; i++){
+        for(int j = 0; j < direita; j++){
+            if (i == j){
+                printf("Cima, Direita\n");
+            }
+        }
+
+    }
+}
+
+/// Movimento cavalo com loops complexos
+void movCavalo(){
+    printf("\nMovimento do Cavalo\n");
+
+    int movCima = 2;
+    int movDireita = 1;
+
+    int cima = 0;
+    int direita = 0;
+
+    for (int c = 0; c <= movCima; c++){
+        for (int d = 0; d <= movDireita; d++){
+            if (cima < movCima){
+                printf("Cima\n");
+                cima++;
+                continue;   
+            }
+            if (cima == movCima && direita < movDireita){
+                printf("Direita\n");
+                direita++;
+            }
+            if (direita >= movDireita) break;
+        }
+        if (cima == movCima && direita == movDireita) break;
+    }
+   
+}
+
+
+
 int main(){
 
-    int movTorre = 5;
-    int movBispo = 5;
-    int movRainha = 8;
+    int casasTorre = 5;
+    int casasBispo = 5;
+    int casasRainha = 8;
 
-    ///Movimento da Torre ultilizando for.
-    printf("Movimento da Torre (5 casa para a direita):\n");
-    for(int i = 1; i <= movTorre; i++){
-        printf("Direita\n");
-    }
+    printf("\nMovimento da torre:\n");
+    movTorre(casasTorre);
 
-    ///Movimento do Bispo ultilizando while.
-    printf("Movimento do Bispo (5 casas na diagonal):\n");
-    int i = 1;
-    while(i <= movBispo){
-        printf("Cima, Direita\n");
-        i++;
-    }
+    printf("\nMovimento do bispo:\n");
+    movBispo(casasBispo);
 
-    ///Movimento da Rainha ultilizando do-while.
-    printf("Movimento da Rainha (8 casas na diagonal):\n");
-    int j = 1;
-    do{
-        printf("Esquerda\n");
-        j++;
-    }while (j <= movRainha);
+    printf("\nMovimento da rainha:\n");
+    movRainha(casasRainha);
 
-    ///Movimento do Cavalo.
+    movCavalo();
+    movBispoAni(5, 5);
     
-    int movCavaloBaixo = 2;
-    int movCavaloEsquerda = 1;
-    
-    printf("\nMovimento do Cavalo (em 'L': 2 casas para baixo, 1 para a esquerda):\n");
 
-    for(int baixo = 0; baixo < movCavaloBaixo; baixo++){
-        printf("Baixo\n");
-        
-        int esquerda = 0;
-        while(esquerda < movCavaloEsquerda && baixo == movCavaloBaixo - 1){///somente na última iteração do for
-            printf("Esquerda\n");
-            esquerda++;
-        }
-    
-    
-    }
-
-
-
-
-    
- 
-
-
-
-
-
-
-    return 0;
+        return 0;
 }
